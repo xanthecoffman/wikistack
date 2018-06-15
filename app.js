ac3bba326f8646b5dbf19582eaaf99f2e064a685
Xanthe Coffman <xanthecoffman@gmail.com>
2018-06-15 11:51:24 -0400

    Intital Setup

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser")
+const app = express();
+const pg = require("pg")
+const { db } = require('./models/index.js');
+
+db.authenticate().
+then(() => {
+  console.log('connected to the database');
+})
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + "/public"));
+
+
+app.get("/", (req, res, next) => {
+  res.send(`<p> "Hello" </p>`);
+})
+
+
+app.listen(3000, () => {
+  console.log("Running!");
+})
